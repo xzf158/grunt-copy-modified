4675a37cfe56fd2fffadebc17b0e96c8844639ba
Terry <terry.xu@me.com>
2015-10-31 14:47:41 +0800

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,87 +1,73 @@
 /*
- * grunt-request-upload
- * https://github.com/xzf158/grunt-request-upload
+ * grunt-copy-modified
+ * https://github.com/xzf158/grunt-copy-modified
  *
- * Copyright (c) 2015 Terry X
+ * Copyright (c) 2015 Terry
  * Licensed under the MIT license.
  */
 
 'use strict';
 
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 
-	// Project configuration.
-	grunt.initConfig({
-		jshint: {
-			all: [
-				'Gruntfile.js',
-				'tasks/*.js',
-				'<%= nodeunit.tests %>',
-			],
-			options: {
-				jshintrc: '.jshintrc',
-			},
-		},
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
 
-		// Before generating any new files, remove any previously-created files.
-		clean: {
-			tests: ['tmp'],
-		},
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp']
+    },
 
-		// Configuration to be run (and then tested).
-		copy_modified: {
-			default_options: {
-				options: {},
-				files: [{
-					expand: true,
-					dot: false,
-					cwd: '',
-					src: [
-						'**/*.*',
-						'!**/*.less',
-						'!**/*.coffee'
-					],
-					dest: ''
-				}]
-			},
-			custom_options: {
-				options: {
-					separator: ': ',
-					punctuation: ' !!!',
-				},
-				files: [{
-					expand: true,
-					dot: false,
-					cwd: 'test',
-					src: [
-						'**/*.*',
-						'!test/newer',
-					],
-					dest: 'test/newer'
-				}]
-			},
-		},
+    // Configuration to be run (and then tested).
+    copy_modified: {
+      default_options: {
+        options: {
+        },
+        files: {
+          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
+        }
+      },
+      custom_options: {
+        options: {
+          separator: ': ',
+          punctuation: ' !!!'
+        },
+        files: {
+          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
+        }
+      }
+    },
 
-		// Unit tests.
-		nodeunit: {
-			tests: ['test/*_test.js'],
-		},
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js']
+    }
 
-	});
+  });
 
-	// Actually load this plugin's task(s).
-	grunt.loadTasks('tasks');
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
 
-	// These plugins provide necessary tasks.
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	// grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-	// Whenever the "test" task is run, first clean the "tmp" dir, then run this
-	// plugin's task(s), then test the result.
-	grunt.registerTask('test', ['copy_modified']); //, 'nodeunit'
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'copy_modified', 'nodeunit']);
 
-	// By default, lint and run all tests.
-	grunt.registerTask('default', ['jshint', 'test']);
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
 
 };
--- README.md
+++ README.md
@@ -1,12 +1,9 @@
-# grunt-copy-modified [![NPM version](https://badge.fury.io/js/grunt-copy-modified.png)](http://badge.fury.io/js/grunt-copy-modified)
+ # grunt-copy-modified
 
-> Grunt copy plugin, only copy modified files.
-
-## Need Help?
-It starts at the very beginning (including how to properly set-up your command-line on Windows and Mac OS X), and goes beyond the official [Getting Started](http://gruntjs.com/getting-started) guide.
+> Grunt copy plugin, only copy modified files
 
 ## Getting Started
-This plugin requires Grunt `~0.4.1`
+This plugin requires Grunt `~0.4.5`
 
 If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
 
@@ -19,3 +16,74 @@ Once the plugin has been installed, it may be enabled inside your Gruntfile with
 ```js
 grunt.loadNpmTasks('grunt-copy-modified');
 ```
+
+## The "copy_modified" task
+
+### Overview
+In your project's Gruntfile, add a section named `copy_modified` to the data object passed into `grunt.initConfig()`.
+
+```js
+grunt.initConfig({
+  copy_modified: {
+    options: {
+      // Task-specific options go here.
+    },
+    your_target: {
+      // Target-specific file lists and/or options go here.
+    },
+  },
+});
+```
+
+### Options
+
+#### options.separator
+Type: `String`
+Default value: `',  '`
+
+A string value that is used to do something with whatever.
+
+#### options.punctuation
+Type: `String`
+Default value: `'.'`
+
+A string value that is used to do something else with whatever else.
+
+### Usage Examples
+
+#### Default Options
+In this example, the default options are used to do something with whatever. So if the `testing` file has the content `Testing` and the `123` file had the content `1 2 3`, the generated result would be `Testing, 1 2 3.`
+
+```js
+grunt.initConfig({
+  copy_modified: {
+    options: {},
+    files: {
+      'dest/default_options': ['src/testing', 'src/123'],
+    },
+  },
+});
+```
+
+#### Custom Options
+In this example, custom options are used to do something else with whatever else. So if the `testing` file has the content `Testing` and the `123` file had the content `1 2 3`, the generated result in this case would be `Testing: 1 2 3 !!!`
+
+```js
+grunt.initConfig({
+  copy_modified: {
+    options: {
+      separator: ': ',
+      punctuation: ' !!!',
+    },
+    files: {
+      'dest/default_options': ['src/testing', 'src/123'],
+    },
+  },
+});
+```
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
